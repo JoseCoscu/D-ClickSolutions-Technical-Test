cb4cb85b58c854134c47d148c798b905bdb6a547
JoseCoscu <coscujjose@gmail.com>
2026-01-21 13:18:59 -0500

    [ADD]dclk_library: library group and security file, borrow and return button, chatter that tracks borrow book

--- dclk_library_extend/__manifest__.py
+++ dclk_library_extend/__manifest__.py
@@ -1,22 +1,25 @@
 {
-    'name': 'Library Management',
+    'name': 'Library Management Ext',
     'version': '18.0.1.0.0',
-    'summary': 'Library book management',
+    'summary': 'Library book management with librarian role',
     'description': """
 Library Management
 ===========================
 - Manage library books
+- Librarian security group
+- Access control by role
     """,
     'author': 'JoseCoscu',
     'website': 'https://github.com/JoseCoscu',
     'category': 'Productivity',
     'depends': [
-        'base',
+        'base','dclk_library', 'mail'
     ],
     'data': [
+        'security/library_groups.xml',
         'security/ir.model.access.csv',
         'views/library_book_views.xml',
-        'data/library_book_demo_data.xml',
+        # 'views/library_menus.xml',
     ],
     'installable': True,
     'application': True,
